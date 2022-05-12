3719f66562e2653e6f821286c0c42f0c3376d218
justus237 <62303964+justus237@users.noreply.github.com>
2022-05-12 17:39:38 +0200

    probably was down to directory changes

--- run_measurement.sh
+++ run_measurement.sh
@@ -25,19 +25,17 @@ coredns_path="../coredns"
 dnsproxy_path="../dnsproxy"
 chrome_path="../chromium"
 
-#coredns
-cd $coredns_path
+
+cd $root_dir && cd $coredns_path
 echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
 ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
 corednsPID=$!
-#dnsproxy
-cd $root_dir
-cd $dnsproxy_path
+
 dns_server_ip=`echo $ip_address2 |awk -F '/' '{print $1}'`
 
 echo "starting dnsproxy with DoQ upstream"
+cd $root_dir && cd $dnsproxy_path
 ip netns exec $namespace1 ./dnsproxy -u "quic://${dns_server_ip}:8853" -v --insecure --ipv6-disabled -l 127.0.0.2 >& $root_dir/dnsproxy.log &
-cd $root_dir
 echo "DoQ: running dig"
 h3_server_ip=$(ip netns exec $namespace1 dig @127.0.0.2 +short www.example.org | tail -n1)
 echo "dig result: www.example.org IN A ${h3_server_ip}"
@@ -59,20 +57,20 @@ echo -n > $root_dir/dnsproxy.log
 echo "killing dnsproxy"
 kill -SIGTERM $dnsproxyPID
 
-echo "killing coredns"
-cp $root_dir/coredns.log $root_dir/coredns-doh.log
-echo -n > $root_dir/coredns.log
-kill -SIGTERM $corednsPID
-cd $coredns_path
-echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
-ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
-corednsPID=$!
+# echo "killing coredns"
+# cp $root_dir/coredns.log $root_dir/coredns-doh.log
+# echo -n > $root_dir/coredns.log
+# kill -SIGTERM $corednsPID
+# cd $root_dir && cd $coredns_path
+# echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
+# ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
+# corednsPID=$!
 
 
+cd $root_dir && cd $dnsproxy_path
 echo "starting dnsproxy with DoH upstream"
 ip netns exec $namespace1 ./dnsproxy -u "https://${dns_server_ip}:443/dns-query" -v --insecure --ipv6-disabled -l 127.0.0.2 >& $root_dir/dnsproxy.log &
 dnsproxyPID=$!
-cd $root_dir
 echo "DoH: running dig"
 h3_server_ip=$(ip netns exec $namespace1 dig @127.0.0.2 +short www.example.org | tail -n1)
 echo "dig result: www.example.org IN A ${h3_server_ip}"
@@ -83,35 +81,37 @@ echo -n > $root_dir/dnsproxy.log
 echo "killing dnsproxy"
 kill -SIGTERM $dnsproxyPID
 
-echo "killing coredns"
-cp $root_dir/coredns.log $root_dir/coredns-doh.log
-echo -n > $root_dir/coredns.log
-kill -SIGTERM $corednsPID
-cd $coredns_path
-echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
-ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
-corednsPID=$!
+# echo "killing coredns"
+# cp $root_dir/coredns.log $root_dir/coredns-doh.log
+# echo -n > $root_dir/coredns.log
+# kill -SIGTERM $corednsPID
+# cd $root_dir && cd $coredns_path
+# echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
+# ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
+# corednsPID=$!
 
 
-# echo "starting dnsproxy with DoUDP upstream"
-# ip netns exec $namespace1 ./dnsproxy -u "${dns_server_ip}:53" -v --insecure --ipv6-disabled -l 127.0.0.2 >& $root_dir/dnsproxy.log &
-# dnsproxyPID=$!
-# cd $root_dir
-# echo "DoUDP: running dig"
-# h3_server_ip=$(ip netns exec $namespace1 dig @127.0.0.2 +short www.example.org | tail -n1)
-# echo "dig result: www.example.org IN A ${h3_server_ip}"
+echo "starting dnsproxy with DoUDP upstream"
+ip netns exec $namespace1 ./dnsproxy -u "${dns_server_ip}:53" -v --insecure --ipv6-disabled -l 127.0.0.2 >& $root_dir/dnsproxy.log &
+dnsproxyPID=$!
+echo "DoUDP: running dig"
+h3_server_ip=$(ip netns exec $namespace1 dig @127.0.0.2 +short www.example.org | tail -n1)
+echo "dig result: www.example.org IN A ${h3_server_ip}"
 
-# #dnsproxyPID=$(ps -e | pgrep dnsproxy)
-# cp $root_dir/dnsproxy.log $root_dir/dnsproxy-doudp.log
-# echo -n > $root_dir/dnsproxy.log
-# echo "killing dnsproxy"
-# kill -SIGTERM $dnsproxyPID
+#dnsproxyPID=$(ps -e | pgrep dnsproxy)
+cp $root_dir/dnsproxy.log $root_dir/dnsproxy-doudp.log
+echo -n > $root_dir/dnsproxy.log
+echo "killing dnsproxy"
+kill -SIGTERM $dnsproxyPID
 
 
 
 echo "running web performance measurement"
 ip netns exec $namespace1 python3 chromium_measurement.py $h3_server_ip $msmID
 
+
+kill -SIGTERM $corednsPID
+kill -SIGTERM $dnsproxyPID
 # restart systemd-resolved
 #systemctl enable systemd-resolved
 #systemctl start systemd-resolved
