c2be6b4c36ab9a7130573ef50b39b733d237c310
justus237 <62303964+justus237@users.noreply.github.com>
2022-05-13 14:18:37 +0200

    try new values

--- netem_4g.sh
+++ netem_4g.sh
@@ -19,19 +19,33 @@ if [ ! -e /var/run/netns/${namespace2} ]; then
     exit 2
 fi
 
-#https://www.opensignal.com/reports/2020/01/usa/mobile-network-experience -> best of all
-# down is ATT, up is TM, latency is ATT
-rtt_half="23.6ms"
-rtt_full="47.2ms"
-rtt_var="1ms"
-packetloss="0.5%"
-download="29.1Mbit"
+# #https://www.opensignal.com/reports/2020/01/usa/mobile-network-experience -> best of all
+# # down is ATT, up is TM, latency is ATT
+# rtt_half="23.6ms"
+# rtt_full="47.2ms"
+# rtt_var="0.5ms"
+# packetloss="0.5%"
+# download="29.1Mbit"
+# peak_download="50Mbit"
+# upload="8.8Mbit"
+# peak_upload="10Mbit"
+# # following https://unix.stackexchange.com/a/100797, upload burst should be 4_400B, and download 14_550B
+# upload_burst="10000b"
+# download_burst="30000b"
+
+#~network link conditioner in macOS
+rtt_half="55"
+rtt_var="0.5ms"
+packetloss="0%"
+download="50Mbit"
 peak_download="50Mbit"
-upload="8.8Mbit"
+upload="10Mbit"
 peak_upload="10Mbit"
-# following https://unix.stackexchange.com/a/100797, upload burst should be 4_400B, and download 14_550B
-upload_burst="10000b"
-download_burst="30000b"
+
+upload_burst="10kb"
+download_burst="50kb"
+
+
 
 #ip netns exec $namespace1 tc qdisc delete dev ptp-$interface1 root
 #ip netns exec $namespace2 tc qdisc delete dev ptp-$interface2 root
@@ -39,8 +53,8 @@ download_burst="30000b"
 
 #ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss rate $upload
 ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
-ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 parent 1: netem delay $rtt_full $rtt_var loss $packetloss
+ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 parent 1: netem delay $rtt_half $rtt_var loss $packetloss
 
 #ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss rate $download
-#ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root handle 1: tbf rate $download burst $download_burst latency 1000ms
-#ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 parent 1: netem delay $rtt_half $rtt_var loss $packetloss
+ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root handle 1: tbf rate $download burst $download_burst latency 1000ms
+ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 parent 1: netem delay $rtt_half $rtt_var loss $packetloss
