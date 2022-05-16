80d7a96b9a5278439018c93361d6976990d63ce6
justus237 <62303964+justus237@users.noreply.github.com>
2022-05-16 18:09:07 +0200

    Update get_websites.sh

--- get_websites.sh
+++ get_websites.sh
@@ -1,3 +1,3 @@
-wget -mpEk www.example.org
-wget -mpEk www.wikipedia.org
-wget -mpEk www.instagram.com
+wget -mpEk --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36" www.example.org
+wget -mpEk --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36" www.wikipedia.org
+wget -mpEk --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36" www.instagram.com
