c5ca7d96e42211370e68a366f4073e1c06dfe898
justus237 <62303964+justus237@users.noreply.github.com>
2022-05-16 13:59:45 +0200

    compare tbf+netem+iptables to pure netem

--- tc_tbf_netem_4g.sh
+++ tc_tbf_netem_4g.sh
@@ -46,6 +46,7 @@ echo "experiment_type=4g" >> vars
 rtt_half="55ms"
 rtt_var="0.5ms"
 packetloss="0.11%"
+packetloss_half="0.055%"
 #half of packet loss
 packetloss_dec="0.00055"
 download="50Mbit"
@@ -59,19 +60,19 @@ download_burst="256kb"
 
 
 
-#ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss rate $upload
-#upload speed
-ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
-#latency to server
-ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 parent 1: netem delay $rtt_half $rtt_var
-#packet loss simulation of netem is kinda broken, use iptables instead?
-#packet loss incoming from client on server
-ip netns exec $namespace2 iptables -s $ip_address1 -A INPUT -m statistic --mode random --probability $packetloss_dec -j DROP
+ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $upload
+# #upload speed
+# ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
+# #latency to server
+# ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 parent 1: netem delay $rtt_half $rtt_var
+# #packet loss simulation of netem is kinda broken, use iptables instead?
+# #packet loss incoming from client on server
+# ip netns exec $namespace2 iptables -s $ip_address1 -A INPUT -m statistic --mode random --probability $packetloss_dec -j DROP
 
-#ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss rate $download
-#download speed
-ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root handle 1: tbf rate $download burst $download_burst latency 1000ms
-#latency to client
-ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 parent 1: netem delay $rtt_half $rtt_var
-#packet loss incoming from server on client
-ip netns exec $namespace1 iptables -s $ip_address2 -A INPUT -m statistic --mode random --probability $packetloss_dec -j DROP
+ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $download
+# #download speed
+# ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root handle 1: tbf rate $download burst $download_burst latency 1000ms
+# #latency to client
+# ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 parent 1: netem delay $rtt_half $rtt_var
+# #packet loss incoming from server on client
+# ip netns exec $namespace1 iptables -s $ip_address2 -A INPUT -m statistic --mode random --probability $packetloss_dec -j DROP
