2c9ae56a0ef060b240d6756a91d3f08bbc2425b4
justus237 <62303964+justus237@users.noreply.github.com>
2022-05-31 13:42:14 +0200

    fix cleanup maybe

--- cleanup_after_measurement.sh
+++ cleanup_after_measurement.sh
@@ -11,13 +11,21 @@ if [ -f vars ]; then
   source vars
   if [ -e /var/run/netns/${namespace1} ]; then
     echo "Removing network namespace ${namespace1}"
-    ip netns pids $namespace1 | xargs kill
+    ip netns pids $namespace1 | xargs -r kill
     ip netns del $namespace1
   fi
   if [ -e /var/run/netns/${namespace2} ]; then
     echo "Removing network namespace ${namespace2}"
-    ip netns pids $namespace2 | xargs kill
+    ip netns pids $namespace2 | xargs -r kill
     ip netns del $namespace2
+  fi
+  if [ -e /var/run/netns/${namespace11} ]; then
+    echo "Removing network namespace ${namespace11}"
+    ip netns del $namespace11
+  fi
+  if [ -e /var/run/netns/${namespace22} ]; then
+    echo "Removing network namespace ${namespace22}"
+    ip netns del $namespace22
   fi
   #ip link del $interface1
   #ip link del $interface2
