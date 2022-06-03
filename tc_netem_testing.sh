00934b52e68d8c80c59e35947fd89de303eff163
justus237 <62303964+justus237@users.noreply.github.com>
2022-06-03 14:03:26 +0200

    rename tc scripts

--- tc_netem_testing.sh
+++ tc_netem_testing.sh
@@ -49,9 +49,9 @@ echo "experiment_type=fixed" >> vars
 #latency mean: 44.5534ms median: 22.52ms
 #latency stdev: 107.4591ms
 
-rtt_half="22.2767"
-rtt="44.5534"
-rtt_stdev="107.4591"
+rtt_half="1"
+rtt="1"
+rtt_stdev="1"
 
 download="70.54765Mbit"
 upload="10.41276Mbit"
@@ -62,14 +62,21 @@ download_burst="256kb"
 
 
 
-#ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $upload
-#upload speed
-ip netns exec $namespace11 tc qdisc add dev $interface12 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
-#latency to server
-ip netns exec $namespace11 tc qdisc add dev $interface12 parent 1: netem rate 1000mbit delay ${rtt} ${rtt_stdev}
+#client -> server
+ip netns exec $namespace11 tc qdisc add dev $interface12 root netem delay $rtt_half $rtt_stdev
 
-#ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $download
-#download speed
-ip netns exec $namespace22 tc qdisc add dev $interface21 root handle 1: tbf rate $download burst $download_burst latency 1000ms
-#latency to client
-#ip netns exec $namespace22 tc qdisc add dev $interface21 parent 1: netem delay $rtt_half $rtt_stdev
+#server -> client
+#ip netns exec $namespace22 tc qdisc add dev $interface21 root netem rate $download
+
+
+# #ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $upload
+# #upload speed
+# ip netns exec $namespace11 tc qdisc add dev $interface12 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
+# #latency to server
+# ip netns exec $namespace11 tc qdisc add dev $interface12 parent 1: netem rate 1000mbit delay ${rtt} ${rtt_stdev}
+
+# #ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $download
+# #download speed
+# ip netns exec $namespace22 tc qdisc add dev $interface21 root handle 1: tbf rate $download burst $download_burst latency 1000ms
+# #latency to client
+# #ip netns exec $namespace22 tc qdisc add dev $interface21 parent 1: netem delay $rtt_half $rtt_stdev
