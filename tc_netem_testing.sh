9db64368b72f8882cc3b11bdecc415970fc1944f
justus237 <62303964+justus237@users.noreply.github.com>
2022-06-03 14:07:02 +0200

    more testing to see where net01 breaks

--- tc_netem_testing.sh
+++ tc_netem_testing.sh
@@ -63,7 +63,7 @@ download_burst="256kb"
 
 
 #client -> server
-ip netns exec $namespace11 tc qdisc add dev $interface12 root netem delay $rtt_half $rtt_stdev
+ip netns exec $namespace1 tc qdisc add dev $interface1 root netem delay $rtt_half $rtt_stdev
 
 #server -> client
 #ip netns exec $namespace22 tc qdisc add dev $interface21 root netem rate $download
