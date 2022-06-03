6f16031ad9e511cd0a4e35593f703370b4c8b662
justus237 <62303964+justus237@users.noreply.github.com>
2022-06-03 13:32:24 +0200

    Update tc_no_netem_fixed_average.sh

--- tc_no_netem_fixed_average.sh
+++ tc_no_netem_fixed_average.sh
@@ -56,19 +56,20 @@ rtt_stdev="107.4591ms"
 download="70.54765Mbit"
 upload="10.41276Mbit"
 
-
+upload_burst="256kb"
+download_burst="256kb"
 
 
 
 
 #ip netns exec $namespace1 tc qdisc add dev ptp-$interface1 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $upload
 #upload speed
-ip netns exec $namespace11 tc qdisc add dev $interface12 root handle 1: tbf rate $upload
+ip netns exec $namespace11 tc qdisc add dev $interface12 root handle 1: tbf rate $upload burst $upload_burst latency 1000ms
 #latency to server
 ip netns exec $namespace11 tc qdisc add dev $interface12 parent 1: netem rate 1000mbit delay $rtt $rtt_stdev
 
 #ip netns exec $namespace2 tc qdisc add dev ptp-$interface2 root netem delay $rtt_half $rtt_var loss $packetloss_half rate $download
 #download speed
-ip netns exec $namespace22 tc qdisc add dev $interface21 root handle 1: tbf rate $download
+ip netns exec $namespace22 tc qdisc add dev $interface21 root handle 1: tbf rate $download burst $download_burst latency 1000ms
 #latency to client
 #ip netns exec $namespace22 tc qdisc add dev $interface21 parent 1: netem delay $rtt_half $rtt_stdev
