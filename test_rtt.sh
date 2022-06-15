fad88bee17a0fec753e6cb5eb5ddb63b33d6af56
justus237 <62303964+justus237@users.noreply.github.com>
2022-06-15 14:24:16 +0200

    fix script maybe

--- test_rtt.sh
+++ test_rtt.sh
@@ -18,6 +18,63 @@ if [[ ! -e /var/run/netns/${namespace2} ]]; then
     exit 2
 fi
 
+coredns_path="../coredns"
+dnsproxy_path="../dnsproxy"
+chrome_path="../chromium"
+
+echo "generating Corefile"
+server_ip=`echo $ip_address2 |awk -F '/' '{print $1}'`
+echo "quic://.:8853 {
+  bind ${server_ip}
+  tls localhost.crt localhost.key {
+    session_ticket_key session_ticket.key
+  }
+  hosts {
+    ${server_ip} www.localdomain.com
+    reload 1h
+  }
+  h3server ${website_under_test}/ ${server_ip}:6121
+  errors
+  log
+  debug
+}
+
+.:53 {
+  bind ${server_ip}
+  hosts {
+    ${server_ip} www.localdomain.com
+    reload 1h
+  }
+  errors
+  log
+  debug
+}
+
+tcp://.:80 {
+  bind ${server_ip}
+  hosts {
+    ${server_ip} www.localdomain.com
+    reload 1h
+  }
+  errors
+  log
+  debug
+}
+
+https://.:443 {
+  bind ${server_ip}
+  tls localhost.crt localhost.key {
+    session_ticket_key session_ticket.key
+  }
+  hosts {
+    ${server_ip} www.localdomain.com
+    reload 1h
+  }
+  errors
+  log
+  debug
+}" | tee "${coredns_path}/Corefile"
+
 cd $root_dir && cd $coredns_path
 #echo "starting coredns for DoQ udp:8853, DoUDP udp:53 and DoH tcp:443"
 ip netns exec $namespace2 ./coredns >& $root_dir/coredns.log &
@@ -39,12 +96,12 @@ client_ip=`echo $ip_address1 |awk -F '/' '{print $1}'`
 
 echo "traceroutes from client to server with first ttl 30"
 echo "TCP"
-ip netns exec $namespace1 traceroute -T -f 30 -p 443 $dns_server_ip
+ip netns exec $namespace1 traceroute -T -f 30 -p 80 $dns_server_ip
 echo "UDP"
 ip netns exec $namespace1 traceroute -U -f 30 -p 53 $dns_server_ip
 echo "hping3"
 echo "TCP"
-ip netns exec $namespace1 hping3 -S -c 1 -p 443 $dns_server_ip
+ip netns exec $namespace1 hping3 -S -c 1 -p 80 $dns_server_ip
 echo "UDP"
 ip netns exec $namespace1 hping3 --udp -c 1 -p 53 $dns_server_ip
 
