236fdc95d0433d68f422484beccb65615c88a958
justus237 <62303964+justus237@users.noreply.github.com>
2022-06-15 14:27:45 +0200

    Update measure_rtt.sh

--- measure_rtt.sh
+++ measure_rtt.sh
@@ -9,25 +9,29 @@ echo "website_under_test=www.example.org" > website-under-test
 
 bash ./setup_measurement.sh >> setup.log 2>&1
 bash ./tc_netem_fixed_dsl.sh
-echo "42.35979ms"
+echo "##DSL: 42.35979ms"
 bash ./test_rtt.sh
+sleep 5
 
 bash ./setup_measurement.sh >> setup.log 2>&1
 bash ./tc_netem_fixed_cable.sh
-echo "25.17665ms"
+echo "##Cable: 25.17665ms"
 bash ./test_rtt.sh
+sleep 5
 
 bash ./setup_measurement.sh >> setup.log 2>&1
 bash ./tc_netem_fixed_fiber.sh
-echo "14.767147ms"
+echo "##Fiber: 14.767147ms"
 bash ./test_rtt.sh
+sleep 5
 
 bash ./setup_measurement.sh >> setup.log 2>&1
 bash ./tc_netem_4g.sh
-echo "91.85231065904635ms"
+echo "##4G: 91.85231065904635ms"
 bash ./test_rtt.sh
+sleep 5
 
 bash ./setup_measurement.sh >> setup.log 2>&1
 bash ./tc_netem_4g_medium.sh
-echo "104.5041425140391ms"
+echo "##4G medium: 104.5041425140391ms"
 bash ./test_rtt.sh
